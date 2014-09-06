79788471c620f11fb523033425b0490c0cf06462
nathanbreitsch <nathanbreitsch@gmail.com>
2014-09-05 21:36:36 -0400

    cleaned up library, did some desk work

--- client/views/desk/desk.js
+++ client/views/desk/desk.js
@@ -2,5 +2,8 @@ Template.desk.helpers({
     fragments: function() {
                    console.log(this.fragments);
                    return Fragments.find({_id: {$in: this.fragments}});
-               }
+               },
+    title: function(){
+        return Session.get("currentStory").title;
+    }
 });
