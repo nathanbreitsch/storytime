b17229d2dfbee3e299ffc28beed2fd21660fae2c
brianbreitsch <brocktane@gmail.com>
2014-09-09 15:40:37 -0400

    changing up the desk

--- client/views/desk/desk.js
+++ client/views/desk/desk.js
@@ -1,9 +1,26 @@
 Template.desk.helpers({
-    fragments: function() {
-                   console.log(this.fragments);
-                   return Fragments.find({_id: {$in: this.fragments}});
-               },
-    title: function(){
-        return Session.get("currentStory").title;
-    }
+  fragments: function() {
+    console.log(this.fragments);
+    return Fragments.find({_id: {$in: this.fragments}});
+  },
+  title: function(){
+           //return this.title;
+    return Session.get("currentStory").title;
+  }
+});
+
+Template.fragmentDialog.events({
+  'click #fragment-submit': function(e, t) {
+    e.preventDefault();
+    var text = t.find('#fragment-submit-text').value;
+    var storyId = this._id;
+    Meteor.call('submitFragment', {text: text, storyId: storyId}, function(e, id) {
+        if (e !== null) {
+          console.log('fragment submit error: ' + e.message);
+          $('#submit-help').text('error submitting fragment: ' + e.message);
+        } else {
+          console.log('fragment submit: ' + id);
+        }
+      });
+    },
 });
--- collections/fragments.js
+++ collections/fragments.js
@@ -1,21 +1,24 @@
 Fragments = new Meteor.Collection("fragments");
 
 Meteor.methods({
-    submitFragment: function(fragmentAttributes) {
+    submitFragment: function(fragmentAttr) {
+        console.log('fragmentAttr: ' + fragmentAttr.text);
         var user = Meteor.user();
-        var story = Stories.findOne(fragmentAttributes.storyId);
         // ensure the user is logged in
-        throw new Meteor.Error(401, 'user: ' + Meteor.user());
-        if(!user) {
+        if (!user) {
             throw new Meteor.Error(401, 'You need to login to submit fragments!');
         }
-        if(!fragmentAttributes.body) {
+        if (!fragmentAttr.text) {
             throw new Meteor.Error(422, 'Please write some content');
         }
-        if(!story) {
-            throw new Meteor.Error(422, 'You must add fragments to a story');
+        if (!fragmentAttr.storyId) {
+          throw new Meteor.Error(422, 'You must add fragments to a story');
         }
-        fragment = _.extend(_.pick(fragmentAttributes, 'storyId', 'body'), {
+        var story = Stories.findOne(fragmentAttr.storyId);
+        if(!story) {
+            throw new Meteor.Error(422, 'Could not find story with corresponding id');
+        }
+        fragment = _.extend(_.pick(fragmentAttributes, 'storyId', 'text'), {
             userId: user._id,
             author: user.username,
             submitted: new Date().getTime()
